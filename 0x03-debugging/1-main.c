f9442632090ab165d1c22efa906b35d2dec4f57d
Faith Kamakia <wfaithmuthoni578@gmail.com>
2023-04-27 12:29:01 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
+
+
 /**
 * main - causes an infinite loop
 * Return: 0
  */
+
+
 int main(void)
+
+
 {
 int i;
 printf("Infinite loop incoming :(\n");
@@ -15,3 +21,4 @@ i = 0;
 printf("Infinite loop avoided! \\o/\n");
 return (0);
 }
+
