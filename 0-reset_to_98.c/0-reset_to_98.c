30f0146b3cea277213a5ad43dd2b949fc66af3cf
Faith Kamakia <wfaithmuthoni578@gmail.com>
2023-05-02 05:04:45 -0700

    0-reset_to_98.c

--- 0-reset_to_98.c/0-reset_to_98.c
+++ 0-reset_to_98.c/0-reset_to_98.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include "main.h"
-/*function that takes pointer to int
+
+/**function that takes pointer to int
  * updates the value it points to 98
  */
 
-int main (void){
+int main(void){
 
 	int n = 98;
 
-	return n;
+	return(n);
 }
