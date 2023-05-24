845441b5d524bd727389e8c14d2bc2e4d49f5548
Faith Kamakia <wfaithmuthoni578@gmail.com>
2023-05-24 03:08:03 -0700

    1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -3,6 +3,9 @@
 #include <stdlib.h>
 #include "function_pointers.h"
 /**
+*array_iterator- iterates over an integer array
+*and performs action on each element
+*@array: pointer to an integer array
 *function that executes a function
 *@size: size of the array
 *@action: pointer to the function
