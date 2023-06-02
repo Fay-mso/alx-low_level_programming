ba3318b91724e12b902117b6e4524c8d57554739
Faith Kamakia <wfaithmuthoni578@gmail.com>
2023-06-02 13:42:12 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
-#ifndef list_h
-#define list_h
+#ifndef lists_h
+#define lists_h
 #include <stdio.h>
 #include <stdlib.h>
 /**
@@ -20,4 +20,4 @@ size_t list_len(const list_t *h);
 size_t print_list(const list_t *h);
 list_t *add_node_end(list_t **head, const char *str);
 list_t *add_node(list_t **head, const char *str);
-#endif
+#endif /*lists_h*/
