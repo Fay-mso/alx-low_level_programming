6e744bedf4cf4b41d1a304aeadccb164ecfa989f
Faith Kamakia <wfaithmuthoni578@gmail.com>
2023-06-02 04:46:15 -0700

    0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "list.h"
+#include <stdbool.h>
+/**
+*print_list - prints all elements of list_h
+*@h: pointer to list_t
+*Return: the number of nodes
+**/
+size_t print_list(const list_t *h)
+{
+const list_t *current = h;
+size_t count = 0;
+if (current == NULL)
+{
+printf("[0] (nil)\n");
+return (count);
+}
+while (current != NULL)
+{
+if (current->str == NULL)
+printf("[0] (nil)\n");
+else
+printf("[%lu] %s\n",(unsigned long)(count + 1), current->str);
+current = current->next;
+count++;
+}
+return (count);
+}
+
