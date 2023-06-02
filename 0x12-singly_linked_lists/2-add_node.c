482b06b312f7c6d29ffb1d05ecf0e30d3097b585
Faith Kamakia <wfaithmuthoni578@gmail.com>
2023-06-02 05:50:01 -0700

    2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,6 +11,9 @@
 list_t *add_node(list_t **head, const char *str)
 {
 list_t *new_node;
+unsigned int len = 0;
+while (str[len])
+len++;
 if (str == NULL)
 return (NULL);
 new_node = malloc(sizeof(list_t));
