53483474004afc28d871603a9b1e182dfeab9b6c
Faith Kamakia <wfaithmuthoni578@gmail.com>
2023-06-02 07:25:31 -0700

    101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,6 +1,4 @@
-section .data
-message db "Hello, Holberton\n", 0
-section .text
+format: db "Hello, Holberton\n", 0
 extern printf
 global main
 main:
@@ -9,29 +7,4 @@ xor   eax, eax
 call  printf
 mov   eax, 0
 ret
-; Save callee-saved registers
-push rbx
-push rbp
-push r12
-push r13
-push r14
-push r15
-; Prepare arguments for printf function call
-mov rdi, message
-xor rax, rax
-; Call printf
-call printf
-; Clean up stack
-add rsp, 8
-; Restore callee-saved registers
-pop r15
-pop r14
-pop r13
-pop r12
-pop rbp
-pop rbx
-; Exit program
-mov eax, 0x60
-xor edi, edi
-syscall
 
